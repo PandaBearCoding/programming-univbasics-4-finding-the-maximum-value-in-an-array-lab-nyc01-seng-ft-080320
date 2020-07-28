fa4c285099de2d04313c3c6121088cbf0333f578
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 17:50:19 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,10 +2,8 @@ def find_max_value(array)
   count = 0
   while count < array.length do
     if array[count] == array.max
-      
-  
-  
-  
-  
+      reutrn count 
+    end
+    count += 1
+  end
 end
-
